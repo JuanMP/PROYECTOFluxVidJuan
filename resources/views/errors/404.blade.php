82e46414d1f5e18c80e07dc79f36f58c1c0ae0e2
Juan Martínez Pérez <juamarper3@alu.edu.gva.es>
2023-12-18 18:45:16 +0100

    actividad 4 acabada LARAVEL - layout

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,5 +6,5 @@
 <h1>Error 404, no se ha podido encontrar el contenido</h1>
 <p>Lo sentimos, la página que estás buscando no existe.
     Puedes probar de nuevo o buscar más información en internet acerca
-    de la página
+    de la página que estás solicitando
 </p>
