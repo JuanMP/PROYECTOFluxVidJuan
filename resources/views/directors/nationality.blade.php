b805ed45da6e1166c78f44ea9312eacf796a313e
Juan Martínez Pérez <juamarper3@alu.edu.gva.es>
2024-01-15 16:25:06 +0100

    ACT 11 - HECHA

--- resources/views/directors/nationality.blade.php
+++ resources/views/directors/nationality.blade.php
@@ -4,8 +4,18 @@
     <h1>País: {{ $country }}</h1>
 
     <ul>
-        @foreach($directors as $director)
+        @foreach ($directors as $director)
             <li>{{ $director->name }}</li>
+
+            @if ($director->movies->count() > 0)
+                <ul>
+                    @foreach ($director->movies as $movie)
+                        <li>{{ $movie->title }}</li>
+                    @endforeach
+                </ul>
+            @else
+                <p>No hay películas</p>
+            @endif
         @endforeach
     </ul>
 @endsection
