70d8133c407030489ade98ef214d359655b76eca
Juan Martínez Pérez <juamarper3@alu.edu.gva.es>
2024-01-24 18:02:15 +0100

    ACT 15 Autenticación - A medias (Haciendo el SignupRequest | Paso 5 PDF)

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+@section('title', 'Página de Login')
+
+@section('content')
+
+<form action="{{ route('login') }}" method="post">
+
+    @csrf
+
+    <label for="username">Nombre de usuario:</label><br>
+    <input type="text" name="username" id="username" value="{{ old('username') }}"><br>
+
+    <label for="password">Contraseña:</label><br>
+    <input type="password" name="password" id="password"><br>
+
+    <input type="submit" value="Enviar">
+</form>
+
+@endsection
