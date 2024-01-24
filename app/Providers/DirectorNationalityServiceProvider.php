b4ad7a005410235d8dd24d8bf483fb6b76dee9c5
Juan Martínez Pérez <juanmartinezclase@gmail.com>
2024-01-24 01:11:21 +0100

    ACT 14 Service Provider - A medias

---
+++ app/Providers/DirectorNationalityServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+use App\Models\Director;
+
+class DirectorNationalityServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+        $countries = Director::select('nationality')->distinct()->get();
+        view()->share('countries', $countries);
+    }
+}
